a4ab29bccb9b4e88b3e70fadff8d7b5490e255dc
litanzheng <tanzheng.li@outlook.com>
2022-11-01 20:40:28 +0800

    第一次提交

---
+++ service_client/service_user_client/src/main/java/com/tencent/yygh/user/client/PatientFeignClient.java
@@ -0,0 +1,16 @@
+package com.tencent.yygh.user.client;
+
+import com.atguigu.yygh.model.user.Patient;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient("service-user")
+@Repository
+public interface PatientFeignClient {
+
+
+    @GetMapping("/api/user/patient/inner/get/{id}")
+    public Patient getPatientOrder(@PathVariable("id") Long id);
+}
---
+++ service_gateway/src/main/java/com/tencent/yygh/gateway/config/CrossConfig.java
@@ -0,0 +1,7 @@
+package com.tencent.yygh.gateway.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class CrossConfig {
+}
